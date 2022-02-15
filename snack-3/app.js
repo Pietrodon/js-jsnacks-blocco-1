e7089c8951904eb3d5b6451bebcadab154b9d850
Pietrodon <donvitopietro00@gmail.com>
2022-02-15 15:32:30 +0100

    snack-3

---
+++ snack-3/app.js
@@ -0,0 +1,10 @@
+let userNumber
+let sum
+
+for (let i=0;i<5;i++){
+    let userNumber = parseInt (prompt('inseriscri un numero'))
+    console.log(userNumber)
+}
+sum += (userNumber)
+    console.log(sum)
+
