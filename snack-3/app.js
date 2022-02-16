98bbd8589cc5b576f8e691b20cdd07a9e941a12d
Pietrodon <donvitopietro00@gmail.com>
2022-02-16 16:32:10 +0100

    snack-9 e modifica snack-3

--- snack-3/app.js
+++ snack-3/app.js
@@ -1,10 +1,11 @@
 let userNumber
-let sum
+let sum = 0;
 
 for (let i=0;i<5;i++){
     let userNumber = parseInt (prompt('inseriscri un numero'))
     console.log(userNumber)
+    sum += (userNumber)
 }
-sum += (userNumber)
+
     console.log(sum)
 
---
+++ snack-9/app.js
@@ -0,0 +1,15 @@
+//dichiaro la variabile somma 
+let sum = 0;
+// dichiaro la variabile media
+let media = 0;
+// prendo i primi 10 numeri partendo da 0
+for (let i=1;i<=10;i++){
+    
+    // faccio la somma e la media  dei numeri
+    sum += i;
+    media = (sum/i)
+    
+}
+// stampo la somma e media dei numeri
+console.log(sum)
+console.log(media)
